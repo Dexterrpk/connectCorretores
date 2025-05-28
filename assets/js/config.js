09215a4cc7a4f693449ecfccc956fe98f5ac5cdd
Dexterrpk <50002067+Dexterrpk@users.noreply.github.com>
2025-05-28 03:50:07 -0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,15 +1,9 @@
-<script type="module">
-   const firebaseConfig = {
-    apiKey: "AIzaSyBIhkg9kqxMqep1gixoI7mkCo6b3j9DnoI",
-    authDomain: "connectcorretores-70cec.firebaseapp.com",
-    projectId: "connectcorretores-70cec",
-    storageBucket: "connectcorretores-70cec.firebasestorage.app",
-    messagingSenderId: "272445820844",
-    appId: "1:272445820844:web:5b7fcd6495d9016767f409",
-    measurementId: "G-M8HKBE7EC7"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+const firebaseConfig = {
+  apiKey: "AIzaSyBIhkg9kqxMqep1gixoI7mkCo6b3j9DnoI",
+  authDomain: "connectcorretores-70cec.firebaseapp.com",
+  projectId: "connectcorretores-70cec",
+  storageBucket: "connectcorretores-70cec.firebasestorage.app",
+  messagingSenderId: "272445820844",
+  appId: "1:272445820844:web:5b7fcd6495d9016767f409",
+  measurementId: "G-M8HKBE7EC7"
+};
